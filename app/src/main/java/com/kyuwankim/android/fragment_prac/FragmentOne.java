c506ee3bfa9654ecd409e272584096871a09b9c7
kyuwankim <lpnids@gmail.com>
2017-04-26 23:37:27 +0900

    add fragment example

---
+++ app/src/main/java/com/kyuwankim/android/fragment_prac/FragmentOne.java
@@ -0,0 +1,19 @@
+package com.kyuwankim.android.fragment_prac;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by kimkyuwan on 2017. 4. 26..
+ */
+public class FragmentOne extends android.app.Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+
+        return inflater.inflate(R.layout.fragment_one, container, false);
+    }
+
+
+}
